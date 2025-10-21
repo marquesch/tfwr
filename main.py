fae30af067c4def20cd3ffe12ed7a46b21ff9ba7
Carlos Marques <carlos.marques@grupo-3c.com>
2025-10-21 13:11:01 -0300

    tomara que de certo

--- main.py
+++ main.py
@@ -1,83 +1,81 @@
 from __builtins__ import *
-
 from util import (
-	build_base_matrix,
-	fertilize_if_has_to_wait,
-	opposite_dir,
-	wait_harvestable,
-	fertilize_if_has_to_wait,
-	water_tile,
+    fertilize_if_has_to_wait,
+    move_to,
+    water_tile,
 )
-from util import plan_execution
-from util import restart_position
-from util import move_to
-from util import right_of
-
-from service import plan_area
 
 
 def farm_basics():
-	while True:
-		water_tile()
-		entity, (x, y) = get_companion()
-		move_to(x, y)
-		current_entity = get_entity_type()
-		current_ground = get_ground_type()
-		if entity == current_entity:
-			fertilize_if_has_to_wait()
-			harvest()
-			plant(entity)
-			continue
+    while True:
+        water_tile()
+        entity, (x, y) = get_companion()
+        move_to(x, y)
+        current_entity = get_entity_type()
+        current_ground = get_ground_type()
+        if entity == current_entity:
+            fertilize_if_has_to_wait()
+            harvest()
+            plant(entity)
+            continue
 
-		if entity == Entities.Grass:
-			if current_ground == Grounds.Soil:
-				fertilize_if_has_to_wait()
-				harvest()
-				till()
-				continue
+        if entity == Entities.Grass:
+            if current_ground == Grounds.Soil:
+                fertilize_if_has_to_wait()
+                harvest()
+                till()
+                continue
 
-			fertilize_if_has_to_wait()
-			harvest()
-			continue
+            fertilize_if_has_to_wait()
+            harvest()
+            continue
 
-		if entity in (Entities.Bush, Entities.Tree):
-			if current_ground == Grounds.Soil:
-				fertilize_if_has_to_wait()
-				harvest()
-				till()
-				plant(entity)
-				continue
+        if entity in (Entities.Bush, Entities.Tree):
+            if current_ground == Grounds.Soil:
+                fertilize_if_has_to_wait()
+                harvest()
+                till()
+                plant(entity)
+                continue
 
-			fertilize_if_has_to_wait()
-			harvest()
-			plant(entity)
-			continue
+            fertilize_if_has_to_wait()
+            harvest()
+            plant(entity)
+            continue
 
-		if current_ground == Grounds.Soil:
-			fertilize_if_has_to_wait()
-			harvest()
-			plant(entity)
-			continue
+        if current_ground == Grounds.Soil:
+            fertilize_if_has_to_wait()
+            harvest()
+            plant(entity)
+            continue
 
-		fertilize_if_has_to_wait()
-		harvest()
-		till()
-		plant(entity)
+        fertilize_if_has_to_wait()
+        harvest()
+        till()
+        plant(entity)
 
 
 def farm_treasure():
-	while True:
-		plant(Entities.Bush)
-		substance = get_world_size() * 2 ** (num_unlocked(Unlocks.Mazes) - 1)
-		use_item(Items.Weird_Substance, substance)
+    map_of_list_of_directions = {
+        North: [East, North, West, South],
+        East: [South, East, North, West],
+        South: [West, South, East, North],
+        West: [North, West, South, East],
+    }
 
-		last_direction = North
-		while not get_entity_type() == Entities.Treasure:
-			while not can_move(last_direction):
-				last_direction = right_of[last_direction]
+    while True:
+        plant(Entities.Bush)
+        substance = get_world_size() * 2 ** (num_unlocked(Unlocks.Mazes) - 1)
+        use_item(Items.Weird_Substance, substance)
 
-			move(last_direction)
-		harvest()
+        last_direction = North
+        while not get_entity_type() == Entities.Treasure:
+            for dir in map_of_list_of_directions[last_direction]:
+                if can_move(dir):
+                    move(dir)
+                    last_direction = dir
+                    break
+        harvest()
 
 
 clear()
--- util.py
+++ util.py
@@ -1,147 +1,148 @@
 from __builtins__ import *
 
 op_dir = {North: South, South: North, West: East, East: West}
-
 right_of = {North: East, East: South, South: West, West: North}
 
 
 def is_even(n):
-	return n % 2 == 0
+    return n % 2 == 0
 
 
 def are_even(x, y):
-	return is_even(x) and is_even(y)
+    return is_even(x) and is_even(y)
 
 
 def is_odd(n):
-	return n % 2 == 1
+    return n % 2 == 1
 
 
 def are_odd(x, y):
-	return is_odd(x) and is_odd(y)
+    return is_odd(x) and is_odd(y)
 
 
 def build_base_matrix(size):
-	base_matrix = []
-	for i in range(size):
-		m = []
-		for j in range(size):
-			m.append([])
-		base_matrix.append(m)
-	return base_matrix
+    base_matrix = []
+    for i in range(size):
+        m = []
+        for j in range(size):
+            m.append([])
+        base_matrix.append(m)
+    return base_matrix
 
 
 def get_move_dir_lambda(dir):
-	def move_dir():
-		move(dir)
+    def move_dir():
+        move(dir)
 
-	return move_dir
+    return move_dir
 
 
 def plan_execution(matrix):
-	execution = []
-	for column in matrix:
-		for tile_jobs in column:
-			for job in tile_jobs:
-				execution.append(job)
-			execution.append(move_n_lambda)
-		execution.append(move_e_lambda)
-	return execution
+    execution = []
+    for column in matrix:
+        for tile_jobs in column:
+            for job in tile_jobs:
+                execution.append(job)
+            execution.append(move_n_lambda)
+        execution.append(move_e_lambda)
+    return execution
 
 
 def get_plant_entity_lambda(entity):
-	def plant_entity():
-		plant(entity)
+    def plant_entity():
+        plant(entity)
 
-	return plant_entity
+    return plant_entity
 
 
 def do_all(lambda_fn, route):
-	for dir in route:
-		lambda_fn()
-		move(dir)
+    for dir in route:
+        lambda_fn()
+        move(dir)
 
 
 def opposite_dir(dir):
-	return op_dir[dir]
+    return op_dir[dir]
 
 
 def move_to(pos_x, pos_y):
-	world_size = get_world_size()
+    world_size = get_world_size()
 
-	cur_pos_x = get_pos_x()
-	inside_distance_x = abs(pos_x - cur_pos_x)
-	wrap_distance_x = abs(inside_distance_x - world_size)
-	distance_x = min(inside_distance_x, wrap_distance_x)
-	dir_x = West
-	if pos_x > cur_pos_x:
-		if inside_distance_x < wrap_distance_x:
-			dir_x = East
-	else:
-		if wrap_distance_x < inside_distance_x:
-			dir_x = East
+    cur_pos_x = get_pos_x()
+    inside_distance_x = abs(pos_x - cur_pos_x)
+    wrap_distance_x = abs(inside_distance_x - world_size)
+    distance_x = min(inside_distance_x, wrap_distance_x)
+    dir_x = West
+    if pos_x > cur_pos_x:
+        if inside_distance_x < wrap_distance_x:
+            dir_x = East
+    else:
+        if wrap_distance_x < inside_distance_x:
+            dir_x = East
 
-	cur_pos_y = get_pos_y()
-	inside_distance_y = abs(pos_y - cur_pos_y)
-	wrap_distance_y = abs(inside_distance_y - world_size)
-	distance_y = min(inside_distance_y, wrap_distance_y)
-	dir_y = South
-	if pos_y > cur_pos_y:
-		if inside_distance_y < wrap_distance_y:
-			dir_y = North
-	else:
-		if wrap_distance_y < inside_distance_y:
-			dir_y = North
+    cur_pos_y = get_pos_y()
+    inside_distance_y = abs(pos_y - cur_pos_y)
+    wrap_distance_y = abs(inside_distance_y - world_size)
+    distance_y = min(inside_distance_y, wrap_distance_y)
+    dir_y = South
+    if pos_y > cur_pos_y:
+        if inside_distance_y < wrap_distance_y:
+            dir_y = North
+    else:
+        if wrap_distance_y < inside_distance_y:
+            dir_y = North
 
-	for i in range(distance_x):
-		move(dir_x)
+    for i in range(distance_x):
+        move(dir_x)
 
-	for i in range(distance_y):
-		move(dir_y)
+    for i in range(distance_y):
+        move(dir_y)
 
 
 def restart_position():
-	move_to(0, 0)
+    move_to(0, 0)
 
 
 def is_harvestable():
-	return get_entity_type() not in (None, Entities.Dead_Pumpkin)
+    return get_entity_type() not in (None, Entities.Dead_Pumpkin)
 
 
 def wait_harvestable():
-	while not can_harvest():
-		pass
-
-
-def fertilize_if_has_to_wait():
-	if is_harvestable():
-		while not can_harvest():
-			use_item(Items.Fertilizer)
+    while not can_harvest():
+        pass
 
 
 def wait_if_harvestable():
-	if not is_harvestable():
-		return
-	wait_harvestable()
+    if not is_harvestable():
+        return
+    wait_harvestable()
+
+
+def fertilize_if_has_to_wait():
+    if not is_harvestable():
+        return
+
+    while not can_harvest():
+        use_item(Items.Fertilizer)
 
 
 def normalize_range(current_range, max_length):
-	if current_range != None:
-		return current_range
+    if current_range != None:
+        return current_range
 
-	return 0, max_length
+    return 0, max_length
 
 
 def till_soil():
-	if get_ground_type() == Grounds.Soil:
-		return
+    if get_ground_type() == Grounds.Soil:
+        return
 
-	till()
+    till()
 
 
 def water_tile():
-	while get_water() <= 0.75:
-		use_item(Items.Water)
+    while get_water() <= 0.75:
+        use_item(Items.Water)
 
 
 move_n_lambda = get_move_dir_lambda(North)
